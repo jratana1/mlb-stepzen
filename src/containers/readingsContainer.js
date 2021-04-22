ec2e45480164577fcace81f62622cceb70cf28fb
John Ratana <jratana1@gmail.com>
2021-04-22 01:44:21 -0400

    added redux

--- src/containers/readingsContainer.js
+++ src/containers/readingsContainer.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import ReadingCardContainer from './readingCardContainer'
-import Reading from '../components/buyCardPack'
 import { useDispatch, useSelector } from 'react-redux'
 
 export function getRandom(arr, n) {
@@ -39,17 +38,44 @@ export function shuffle(array) {
 
 export default function ReadingsContainer(props)  {
 const [deck, setDeck] = useState([]);
+const flag = useSelector(state => state.readings.flag);
+const dispatch = useDispatch();
 
-let pack =[]
-pack = getRandom(props.players.players,5)
-let Cards = <></>
+
+
+// let pack =[]
+// pack = getRandom(props.players.players,5)
+// let Cards = <></>
         
-    if ( props.players ) {        
-        Cards = pack.map((player) => <ReadingCardContainer key= {player.id} props={player} />)
-    }
+//     if ( props.players ) {       
+//         Cards = pack.map((player) => <ReadingCardContainer key= {player.id} props={player} x="600" y="-222"/>)
+//     }
     
+  const newPack = () => {
+    let pack = []
+    pack = getRandom(props.players.players,5)
+ 
+    setDeck([
+      ...deck,
+      <ReadingCardContainer key= {pack[0].id} props={pack[0]}  x="0" y="0" />,
+      <ReadingCardContainer key= {pack[1].id} props={pack[1]}  x="0" y="0" />,
+      <ReadingCardContainer key= {pack[2].id} props={pack[2]}  x="0" y="0" />,
+      <ReadingCardContainer key= {pack[3].id} props={pack[3]}  x="0" y="0" />,
+      <ReadingCardContainer key= {pack[4].id} props={pack[4]}  x="0" y="0" />
+
+    ])
+
+  }
+
+  const onCardClick = (event) => { 
+    newPack()
+    
+  }
+
     return <div className="Reading-Container">
-              {Cards}
+              {/* {Cards} */}
+              {deck}
+              <button onClick={(e)=>onCardClick()}>Click Here to Buy a Pack</button>
           </div>
  
 }
